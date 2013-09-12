0edc8a1d5901d12402a9fad07971e2f1d2ca4804
Peter Andersson <pelleplutt1976@gmail.com>
2013-09-12 21:01:37 +0200

    some reminders

--- src/test/testsuites.c
+++ src/test/testsuites.c
@@ -8,7 +8,7 @@
 #include "testrunner.h"
 
 void add_suites() {
-  ADD_SUITE(dev_tests);
-  //ADD_SUITE(check_tests);
-  //ADD_SUITE(hydrogen_tests)
+  //ADD_SUITE(dev_tests);
+  ADD_SUITE(check_tests);
+  ADD_SUITE(hydrogen_tests)
 }
